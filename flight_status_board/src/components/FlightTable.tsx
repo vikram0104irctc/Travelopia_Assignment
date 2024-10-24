3e9af3a00dd91e641f53c67f046db18353b5ebb5
Vikram Kumar Choudhary <vikram0104irctc@gmail.com>
2024-10-24 10:52:36 +0530

    Updated : Added pagination in Flight Table

--- flight_status_board/src/components/FlightTable.tsx
+++ flight_status_board/src/components/FlightTable.tsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { getFormatedTime, getStatusColor } from "../utils/formateDate";
 import { useNavigate } from "react-router-dom";
 import { FlightState } from "../redux/reducer";
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { MdOutlineNavigateBefore } from "react-icons/md";
 
 export const FlightTable = () => {
   const data = useSelector((state: FlightState) => state.flightData);
   const navigate = useNavigate();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 10;
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+  const totalPages = Math.ceil(data.length / recordsPerPage);
+
   const handleFlightRedirect = (id: string) => {
     navigate(`/flight/${id}`);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
   return (
@@ -18,24 +42,15 @@ export const FlightTable = () => {
             <tr className="bg-black text-white text-sm leading-normal">
               <th className="py-3 pr-2 pl-4 text-left">Flight Number</th>
               <th className="py-3 px-4 text-left">Airline</th>
-              <th className="py-3 px-4 text-left hidden sm:table-cell">
-                Origin
-              </th>
-              <th className="py-3 px-4 text-left hidden sm:table-cell">
-                Destination
-              </th>
-              <th className="py-3 px-4 text-left hidden sm:table-cell">
-                Departure Time & Date
-              </th>
+              <th className="py-3 px-4 text-left hidden sm:table-cell">Origin</th>
+              <th className="py-3 px-4 text-left hidden sm:table-cell">Destination</th>
+              <th className="py-3 px-4 text-left hidden sm:table-cell">Departure Time & Date</th>
               <th className="py-3 px-4 text-left">Status</th>
             </tr>
           </thead>
           <tbody className="text-gray-600 text-sm  font-light">
-            {data.map((flight) => (
-              <tr
-                key={flight.id}
-                className="border-b border-gray-200 hover:bg-gray-100"
-              >
+            {currentRecords.map((flight) => (
+              <tr key={flight.id} className="border-b border-gray-200 hover:bg-gray-100">
                 <td className="py-3 px-4">
                   <button
                     className="cursor-pointer underline text-blue-400"
@@ -46,24 +61,15 @@ export const FlightTable = () => {
                 </td>
                 <td className="py-3 px-4">
                   {flight.airline}
-
                   <div className="md:hidden table-cell text-[10px]">
                     {getFormatedTime(flight.departureTime)}
                   </div>
                 </td>
-                <td className="py-3 px-4 hidden sm:table-cell">
-                  {flight.origin}
-                </td>
-                <td className="py-3 px-4 hidden sm:table-cell">
-                  {flight.destination}
-                </td>
+                <td className="py-3 px-4 hidden sm:table-cell">{flight.origin}</td>
+                <td className="py-3 px-4 hidden sm:table-cell">{flight.destination}</td>
                 <td className="py-3  pl-4 pr-3 hidden sm:table-cell">
                   {getFormatedTime(flight.departureTime)} ,
-                  {flight.departureTime
-                    .split("T")[0]
-                    .split("-")
-                    .reverse()
-                    .join("-")}
+                  {flight.departureTime.split("T")[0].split("-").reverse().join("-")}
                 </td>
                 <td className={`py-3 px-4`}>
                   <div
@@ -78,6 +84,26 @@ export const FlightTable = () => {
             ))}
           </tbody>
         </table>
+      </div>
+
+      <div className="flex justify-center gap-4 items-center mt-4 px-2">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+          className="bg-gray-400 px-4 py-1 md:py-2 rounded disabled:opacity-50"
+        >
+          <MdOutlineNavigateBefore />
+        </button>
+        <span className="text-sm">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="bg-gray-400 px-4 py-1 md:py-2 rounded disabled:opacity-50"
+        >
+          <MdOutlineNavigateNext />
+        </button>
       </div>
     </>
   );
